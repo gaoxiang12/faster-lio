fb7bdd51533d4fca35a7eac0104d0e766d055b35
lurvelly <mit-lly@foxmail.com>
2023-12-07 07:25:55 +0000

    Fixed issue that offline mode can't work with Hesai Pandar LiDAR.

--- src/laser_mapping.cc
+++ src/laser_mapping.cc
@@ -108,7 +108,7 @@ bool LaserMapping::LoadParams(ros::NodeHandle &nh) {
     } else if (lidar_type == 4) {
         preprocess_->SetLidarType(LidarType::HESAIxt32);
         LOG(INFO) << "Using Hesai Pandar 32 Lidar";
-    }else {
+    } else {
         LOG(WARNING) << "unknown lidar_type";
         return false;
     }
@@ -202,6 +202,9 @@ bool LaserMapping::LoadParamsFromYAML(const std::string &yaml_file) {
     } else if (lidar_type == 3) {
         preprocess_->SetLidarType(LidarType::OUST64);
         LOG(INFO) << "Using OUST 64 Lidar";
+    } else if (lidar_type == 4) {
+        preprocess_->SetLidarType(LidarType::HESAIxt32);
+        LOG(INFO) << "Using Hesai Pandar 32 Lidar";
     } else {
         LOG(WARNING) << "unknown lidar_type";
         return false;
